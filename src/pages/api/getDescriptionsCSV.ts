3e6446b799e83bcd763ae34f15bc67a16df80212
Archi00 <albert.archi@gmail.com>
2022-11-03 09:35:47 +0100

    update

--- src/pages/api/getDescriptionsCSV.ts
+++ src/pages/api/getDescriptionsCSV.ts
@@ -2,6 +2,6 @@ import fs from "fs"
 import { NextApiRequest, NextApiResponse } from "next"
 
 export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
-    const file = fs.readFileSync(`src/tables/csv/products.csv`)
+    const file = fs.readFileSync(`src/tables/csv/products_combinations_sql.csv`)
     return res.status(200).json(file.toString())
 }
--- src/pages/api/saveDescriptionsCSV.ts
+++ src/pages/api/saveDescriptionsCSV.ts
@@ -5,7 +5,7 @@ import { makeDescriptionsCSV } from "@/utils/helpers"
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
         const writeData = makeDescriptionsCSV(req.body["data"])
-        // const nullData = `ID;Description\n${Object.keys(req.body["data"]).map(num => num + ";").join("\n")}`
+        //const nullData = `ID;Description\n${Object.keys(req.body["data"]).map(num => num + ";").join("\n")}`
         fs.writeFileSync(`src/tables/csv/${req.body["lang"]}Description.csv`, writeData, "utf8")
         return res.status(200).json({message: 200})
     } catch(e) {
