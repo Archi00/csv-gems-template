349d51637ea056f83116567dd5e87f66a2b3b969
Archi00 <albert.archi@gmail.com>
2022-11-28 09:34:10 +0100

    fix: updateIG pythoon

--- src/python/sendCSV.py
+++ src/python/sendCSV.py
@@ -3,22 +3,26 @@ import os
 from dotenv import load_dotenv
 import zipfile
 
-load_dotenv()
-FTP_SERVER=os.getenv("FTP_SERVER")
-FTP_USER=os.getenv("FTP_USER")
-FTP_PWD=os.getenv("FTP_PWD")
-FTP_UPLOAD_PATH=os.getenv("FTP_CSV_PUBLIC_PATH")
-FTP_CSV_PATH=os.getenv("FTP_DOWNLOADS")
+def main():
+    load_dotenv()
+    FTP_SERVER=os.getenv("FTP_SERVER")
+    FTP_USER=os.getenv("FTP_USER")
+    FTP_PWD=os.getenv("FTP_PWD")
+    FTP_UPLOAD_PATH=os.getenv("FTP_CSV_PUBLIC_PATH")
+    FTP_CSV_PATH=os.getenv("FTP_DOWNLOADS")
 
-print(FTP_CSV_PATH)
-with zipfile.ZipFile("{0}/{1}".format(FTP_CSV_PATH, "shop_data_export.zip"), 'r') as zip_ref:
-    print(zip_ref)
-    zip_ref.extractall("public/assets/datafeed")
+    print(FTP_CSV_PATH)
+    with zipfile.ZipFile("{0}/{1}".format(FTP_CSV_PATH, "shop_data_export.zip"), 'r') as zip_ref:
+        print(zip_ref)
+        zip_ref.extractall("public/assets/datafeed")
 
-session = ftplib.FTP(FTP_SERVER,FTP_USER,FTP_PWD)
-file = open("public/assets/datafeed/products_combinations_sql.csv", "rb")
-print(FTP_UPLOAD_PATH)
-session.storbinary("STOR {0}".format(FTP_UPLOAD_PATH), file)
-file.close()
-session.quit()
-os.remove("{0}/{1}".format(FTP_CSV_PATH, "shop_data_export.zip"))
+    session = ftplib.FTP(FTP_SERVER,FTP_USER,FTP_PWD)
+    file = open("public/assets/datafeed/products_combinations_sql.csv", "rb")
+    print(FTP_UPLOAD_PATH)
+    session.storbinary("STOR {0}".format(FTP_UPLOAD_PATH), file)
+    file.close()
+    session.quit()
+    os.remove("{0}/{1}".format(FTP_CSV_PATH, "shop_data_export.zip"))
+    return 0
+
+main()
