ed929e3623094bfae37a909441308a1d433434f8
Archi00 <albert.archi@gmail.com>
2022-12-16 12:56:26 +0100

    fix: export api

--- src/pages/api/exportProducts.ts
+++ src/pages/api/exportProducts.ts
@@ -5,10 +5,24 @@ type Data = {
     name: string
 }
 
+const metaComp = (gem) => {
+	const ret = {}
+	ret["id"] = gem["Product number"]
+	ret["title"] = gem["Product name"]
+	ret["description"] = gem["Short description"]
+	ret["availability"] = "in stock"
+	ret["condition"] = "new"
+	ret["price"] = "0"
+	ret["link"] = gem["Link"]
+	ret["image_link"] = gem["Image URL"].split(",")[0]
+	ret["brand"] = "Gemmesterra"
+	return ret 
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-    const file = fs.readFileSync(`src/utils/exportEverything.txt`, "utf8")
+    const file = fs.readFileSync(`src/utils/exportProducts.txt`, "utf8")
     const result = await executePSQuery({query: file.toString(), values:[]})
-    const activeGems = result.filter(gem => gem["Active"] == 1)
+    const activeGems = result.filter(gem => gem["Active"] == 1).map(gem => metaComp(gem))
     const data = JSON.stringify(activeGems, null, 4)
     fs.writeFileSync(`public/assets/products/products.json`,data)
     return res.status(200).json(JSON.parse(data))
