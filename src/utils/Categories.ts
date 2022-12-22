99fb298764be3fcec62f195cbee90318975df231
Archi00 <albert.archi@gmail.com>
2022-12-22 10:55:00 +0100

    update: cats

--- src/utils/Categories.ts
+++ src/utils/Categories.ts
@@ -120,11 +120,8 @@ export const Categories: any = {
                     ALEXANDRITA: 169,
                     "CANVI DE COLOR": 170,
                     "ULL DE GAT": 171,
-<<<<<<< HEAD
                     LARIMAR: 63,
                     // "ULL DE GAT": 172,
-=======
->>>>>>> refs/remotes/origin/main
                     APATITA: 189,
                     ROBI: 198,
                 },
